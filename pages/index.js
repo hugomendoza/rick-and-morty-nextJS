24e38dadafda021edef263e50811960befb7c91d
Hugo Mendoza <hmendoza@serempre.com>
2020-08-12 15:15:30 -0500

    app rick and morty

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,82 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import Link from "next/link"
 
-export default function Home() {
+const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function Home({ data }) {
+  console.log("data", data);
+  const { info, results: defaultResults = [] } = data;
+  const [results, updateResults] = useState(defaultResults);
+  const [page, updatePage] = useState({
+    ...info,
+    current: defaultEndpoint
+  });
+  const { current } = page;
+
+  useEffect(() => {
+    if ( current === defaultEndpoint ) return;
+
+    async function request() {
+      const res = await fetch(current)
+      const nextData = await res.json();
+
+      updatePage({
+        current,
+        ...nextData.info
+      });
+
+      if ( !nextData.info?.prev ) {
+        updateResults(nextData.results);
+        return;
+      }
+
+      updateResults(prev => {
+        return [
+          ...prev,
+          ...nextData.results
+        ]
+      });
+    }
+    request();
+  }, [current]);
+
+  function handleLoadMore() {
+    updatePage(prev => {
+      return {
+        ...prev,
+        current: page?.next
+      }
+    });
+  }
+
+  function handleOnSubmitSearch(e) {
+    e.preventDefault();
+
+    const { currentTarget = {} } = e;
+    const fields = Array.from(currentTarget?.elements);
+    const fieldQuery = fields.find(field => field.name === "query");
+
+    const value = fieldQuery.value || "";
+    const endpoint = `https://rickandmortyapi.com/api/character/?name=${value}`;
+
+    updatePage({
+      current: endpoint
+    });
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,43 +86,41 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Wubba Lubba Dub Dub!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Rick and Morty Character Wiki
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <form
+          className={styles.search}
+          onSubmit={handleOnSubmitSearch}
+        >
+          <input name="query" type="search" />
+          <button>Search</button>
+        </form>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ul className={styles.grid}>
+          {results.map(result => {
+            const { id, name, image } = result;
+            return (
+              <li key={id} className={styles.card}>
+                <Link href="/character/[id]" as={`/character/${id}`}>
+                  <a>
+                  <img src={image} alt={`${name} Thumbnail`} />
+                    <h3>{ name }</h3>
+                  </a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+        <p>
+          <button
+            onClick={handleLoadMore}
+          >Load More</button>
+        </p>
       </main>
 
       <footer className={styles.footer}>
